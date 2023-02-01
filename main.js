c3bc61be067b3655afa99e43f3ec44b8258a29ce
Tiago Serra <35219940+N2late@users.noreply.github.com>
2023-02-01 13:27:32 +0100

    Class & Style Binding; Computed Properties; Components & Props; Communicating Events; Forms & v-model.

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 0,
-      product: 'Socks',
-      image: './assets/images/socks_green.jpg',
-      inStock: true,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      variants: [
-        {
-          id: 2234,
-          color: 'green',
-          image: './assets/images/socks_green.jpg',
-        },
-        {
-          id: 2235,
-          color: 'blue',
-          image: './assets/images/socks_blue.jpg',
-        },
-      ],
+      cart: [],
+      premium: false,
     };
   },
-  method: {
-    addToCart() {
-      this.cart += 1;
-    },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
     },
   },
 });
